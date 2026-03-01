edb4e8978508ccdac4c6729109ac16c2da90a93e
emafree <pierrot.bru@gmail.com>
2026-03-01 15:33:26 +0100

    Inizio GUI con SLINT

--- ritmo_gui/src/main.rs
+++ ritmo_gui/src/main.rs
@@ -1,22 +1,7 @@
-mod app;
-mod config;
-mod events;
-mod state;
-mod ui;
+slint::include_modules!();
 
-use app::App;
-
-fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([1200.0, 800.0])
-            .with_title("Ritmo - Library Manager"),
-        ..Default::default()
-    };
-    
-    eframe::run_native(
-        "Ritmo",
-        options,
-        Box::new(|cc| Box::new(App::new(cc))),
-    )
+fn main() -> anyhow::Result<()> {
+    let win = MainWindow::new()?;
+    win.run()?;
+    Ok(())
 }
